2b1c097f2e9e6366df34723ccd61f92283ea7c7d
Ramazani Mwemedi <ramazanimwemedi234@gmail.com>
2024-01-18 19:47:47 +0300

    Added a page about c

---
+++ docs/introduction-to-computers/what-is-a-computer.md
@@ -0,0 +1,79 @@
+---
+sidebar_position: 1
+---
+
+# What is a Computer ?
+
+## Demystifying the Magic of Computers
+
+Imagine a computer as a smart and fast friend who can follow your instructions to do various tasks. It's not a magical box; it's a tool that helps you with everything from writing letters and playing games to learning new things. Let's dive into the simple parts that make up this friendly gadget.
+
+1. #### The Brain: Central Processing Unit (CPU):
+
+   - Think of the CPU as the brain of the computer. It's like the boss who decides what needs to be done. When you click on something or type a message, the CPU makes it happen.
+
+2. #### Memory (RAM):
+
+   - RAM is like the computer's short-term memory. It helps the computer work on things quickly. When you open a program or a website, the computer uses RAM to keep it running smoothly.
+
+3. #### Storage:
+
+   - Imagine storage as a drawer where you keep your important stuff. It's where the computer stores everything, from the pictures you take to the software it needs to run.
+
+4. #### Input Devices (Keyboard and Mouse):
+
+   - The keyboard and mouse are how you talk to your computer. Just like writing or pointing, you use these devices to tell the computer what you want to do.
+
+5. #### Output Devices (Monitor):
+
+   - The monitor is like a computer's TV screen. It shows you what's happening—whether it's a game, a movie, or your homework.
+
+6. #### Internet Connection:
+
+   - The internet is like a super-fast postman. It helps your computer talk to other computers around the world. You can use it to send emails, watch videos, or look up interesting things.
+
+## Types of Computers:
+
+1.  #### Personal Computers (PCs):
+
+- PCs are like your own personal helpers. They can be big (desktops) or small (laptops) and are perfect for everyday tasks like writing, playing games, and exploring the internet.
+
+2.  #### Server:
+
+- Think of a server as a helpful friend who stores and shares information. It's like a big computer that helps other computers connect and work together, especially in big offices or on the internet.
+
+3.  #### Mainframe:
+
+- A mainframe is like a super-strong computer used by big companies. It can handle lots of tasks at the same time and is great for important jobs like keeping track of money or managing big websites.
+
+4.  #### Supercomputer:
+
+- Supercomputers are like superheroes of the computer world. They are incredibly powerful and are used for solving really hard problems, like predicting the weather or doing complex scientific calculations.
+
+## What Does a Computer Do?
+
+1.  Processing Information:
+
+    - When you tell the computer to do something, it processes that information using its brain (CPU) and follows your instructions.
+
+2.  Storing Things:
+
+    - Computers are excellent at keeping your files safe. It's like having a virtual storage space where you can keep pictures, documents, and everything else.
+
+3.  Communicating:
+
+    - Computers can help you talk to your friends through messages, emails, or video calls. They can also connect to other computers all around the world.
+
+## How Computers Have Changed:
+
+1.  Early Computers (1940s-1950s):
+
+    - Imagine the first computers as giant calculators. They were big, slow, and mostly used for complex math problems.
+
+2.  Personal Computers (1980s-Present):
+
+    - Now, computers are smaller, faster, and personal. You can have your own computer at home to do homework, play games, and explore the world.
+
+## Conclusion:
+
+In a nutshell, a computer is like a helpful friend that listens to your commands and helps you with tasks. It's not complicated; it's a tool that makes life easier. So, whether you're typing a story, playing games, or talking to friends, your computer is there to make things fun and simple.
---
+++ docusaurus.config.ts
@@ -0,0 +1,208 @@
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+
+const config: Config = {
+  title: "My Site",
+  tagline: "Dinosaurs are cool",
+  favicon: "img/pde-logo.png",
+
+  // Set the production url of your site here
+  url: "https://your-docusaurus-site.example.com",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "facebook", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      {
+        docs: {
+          sidebarPath: "./sidebars.ts",
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        },
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: "img/pde-logo.png",
+    navbar: {
+      title: "PDE ",
+      logo: {
+        alt: "PDE Logo",
+        src: "img/pde-logo.png",
+      },
+      items: [
+        {
+          type: "docSidebar",
+          sidebarId: "tutorialSidebar",
+          position: "left",
+          label: "Courses",
+        },
+        { to: "/blog", label: "Blog", position: "left" },
+
+        {
+          href: "https://github.com/Codust-SIR",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          items: [
+            {
+              html: `
+                <a
+          style="
+            display: flex;
+            flex: 0.4;
+            gap: 10px;
+            color: white;
+            text-decoration: none;
+          "
+          href="https://sirafrica.org"
+          underline="none"
+        >
+          <img
+            height={100}
+            width={100}
+            alt="SIR"
+            src="/img/sirafrica_logo_bw.png"
+            style="
+              height: 45px;
+              width: 45px;
+            "
+          />
+          <div>
+            <h3
+              variant="h5"
+              component="h5"
+              style="
+                flex-grow: 1;
+                color: white;
+                font-weight: 800px;
+                letter-spacing: 3;
+              "
+            >
+              SIR
+            </h3>
+            <p
+              variant="body2"
+              component="p"
+              style={{
+                flexGrow: 1,
+                letter-spacing: 1,
+                color: white,
+              }}
+            >
+              Learn, Earn, Innovate
+            </p>
+          </div>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+        </a>
+                `,
+            },
+          ],
+        },
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "Tutorial",
+              to: "/docs/intro",
+            },
+          ],
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "Twitter",
+              href: "https://twitter.com/SolidarityInit1",
+            },
+            {
+              label: "WhatsApp",
+              href: "https://chat.whatsapp.com/F4kTZOgxoiM2AD475drWqS",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "Blog",
+              to: "/blog",
+            },
+            {
+              label: "GitHub",
+              href: "https://github.com/Codust-SIR",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Solidarity Initiative for Refugees`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+
+    docsSidebars: {
+      // Display the "Basic" sidebar when on a page in the "Basic" section
+      basic: [
+        {
+          type: "autogenerated",
+          dirName: "./docs/basic-ict",
+        },
+      ],
+      // Display the "Advanced" sidebar when on a page in the "Advanced" section
+      advanced: [
+        {
+          type: "autogenerated",
+          dirName: "./docs/advanced-ict",
+        },
+      ],
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
